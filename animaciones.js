a263e307fc3d871c89dea9728affe01129c31ef2
tiagoomontero <tiagoakd10@gmail.com>
2024-10-28 12:45:55 -0300

    carga inicial
    archivos hechos primera carga

---
+++ animaciones.js
@@ -0,0 +1,16 @@
+<script>
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const navMenu = document.getElementById('navMenu');
+
+    // Función para mostrar/ocultar el menú en dispositivos móviles
+    hamburgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+</script>
+
+document.body.addEventListener('click', function() {
+    const video = document.getElementById('myVideo');
+    video.play().catch(function(error) {
+        console.log('Error al reproducir video:', error);
+    });
+});
